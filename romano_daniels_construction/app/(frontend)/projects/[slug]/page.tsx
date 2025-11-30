bd3e3b3e0c4de25f02f9d8a5b90f31771ac01d65
saisaynoomleng-dev <snoomleng1@cougars.ccis.edu>
2025-11-30 18:39:31 -0500

    finish project page

--- romano_daniels_construction/app/(frontend)/projects/[slug]/page.tsx
+++ romano_daniels_construction/app/(frontend)/projects/[slug]/page.tsx
@@ -1,7 +1,73 @@
-import React from 'react';
+import Bounded from '@/components/Bounded';
+import { formatDate } from '@/lib/utils';
+import { urlFor } from '@/sanity/lib/image';
+import { sanityFetch } from '@/sanity/lib/live';
+import { PROJECT_QUERY } from '@/sanity/lib/queries';
+import { PortableText } from 'next-sanity';
+import Image from 'next/image';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+import { FaArrowLeft } from 'react-icons/fa';
 
-const ProjectDetailPage = () => {
-  return <div>ProjectDetailPage</div>;
+const ProjectDetailPage = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const { data: project } = await sanityFetch({
+    query: PROJECT_QUERY,
+    params: await params,
+  });
+
+  if (!project) notFound();
+
+  return (
+    <Bounded className="grid md:grid-cols-2 gap-5">
+      <div className="col-span-full">
+        <Link href="/projects" className="flex items-center gap-x-2 group">
+          <FaArrowLeft className="group-hover:-translate-x-1 transition-transform duration-200" />
+          Back to all projects
+        </Link>
+      </div>
+      <div>
+        {project.mainImage.asset?.url ? (
+          <Image
+            src={urlFor(project.mainImage.asset.url)
+              .width(1000)
+              .height(1000)
+              .auto('format')
+              .format('webp')
+              .url()}
+            width={1000}
+            height={1000}
+            alt={project.mainImage.alt as string}
+            priority
+            loading="eager"
+          />
+        ) : null}
+      </div>
+
+      <div className="flex flex-col gap-y-3">
+        <h2 className="font-semibold">{project.name}</h2>
+        {project.startDate && (
+          <p>
+            <span className="font-semibold">Started Date: </span>
+            {formatDate(project.startDate)}
+          </p>
+        )}
+        {project.finishDate && (
+          <p>
+            <span className="font-semibold">Finish Date: </span>
+            {formatDate(project.finishDate)}
+          </p>
+        )}
+
+        <div className="prose-sm md:prose-lg">
+          {project.desc ? <PortableText value={project.desc} /> : null}
+        </div>
+      </div>
+    </Bounded>
+  );
 };
 
 export default ProjectDetailPage;
--- romano_daniels_construction/app/(frontend)/projects/page.tsx
+++ romano_daniels_construction/app/(frontend)/projects/page.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import Bounded from '@/components/Bounded';
+import ContactUsSection from '@/components/ContactUsSection';
+import ProjectCard from '@/components/ProjectCard';
+import Subtitle from '@/components/Subtitle';
+import { sanityFetch } from '@/sanity/lib/live';
+import { ALL_PROJECTS_QUERY } from '@/sanity/lib/queries';
 
-const ProjectsPage = () => {
-  return <div>ProjectsPage</div>;
+const ProjectsPage = async () => {
+  const { data: projects } = await sanityFetch({ query: ALL_PROJECTS_QUERY });
+
+  return (
+    <Bounded>
+      <div className="flex items-end-safe h-[40vh] project-hero-mobile md:project-hero-tablet lg:project-hero-desktop text-brand-white">
+        <div className="ml-5 pl-5 my-3 border-l-2 border-brand-white">
+          <Subtitle title="our projects" />
+          <h1 className="font-semibold">We build projects that last</h1>
+        </div>
+      </div>
+
+      <div className="grid md:grid-cols-3 place-items-center gap-3">
+        {projects.map((project) => (
+          <ProjectCard key={project.slug.current} {...project} />
+        ))}
+      </div>
+
+      <div>
+        <ContactUsSection />
+      </div>
+    </Bounded>
+  );
 };
 
 export default ProjectsPage;
